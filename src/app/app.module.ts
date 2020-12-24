cf069fa69ce51966e948a239668bdf6b2ec56014
anhamt <75478354+anhamt@users.noreply.github.com>
2020-12-24 11:09:56 +0800

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+@@ -3,11 +3,18 @@ import { NgModule } from '@angular/core';
 
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { HomeComponent } from './components/home/home.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
+	NavbarComponent
+   AppComponent,
    NavbarComponent,
    AboutComponent,
    ContactComponent,
@@ -15,9 +19,3 @@ import { HomeComponent } from './components/home/home.component';
 
   ],
   imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
